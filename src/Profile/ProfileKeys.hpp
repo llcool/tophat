70468396d5e3917b1fc40adbe3e1a7ae8c90308a
lloyd <lloydcbailey@gmail.com>
2015-06-22 12:06:45 +0100

    Adds the vehicle name to the LiveTrack24 data. See: http://bugs.xcsoar.org/ticket/3520 for more information.

--- src/Profile/ProfileKeys.hpp
+++ src/Profile/ProfileKeys.hpp
@@ -251,6 +251,7 @@ extern const char FlarmLocation[];
 
 extern const char TrackingInterval[];
 extern const char TrackingVehicleType[];
+extern const char TrackingVehicleName[];
 extern const char SkyLinesTrackingEnabled[];
 extern const char SkyLinesTrackingInterval[];
 extern const char SkyLinesTrafficEnabled[];
